adbc2bd58275ccbff14175b31b84d264dd4d0d78
MarauderKnight3 <marauderknight3@gmail.com>
2023-12-17 21:16:49 -0600

    Commands setup

---
+++ Command.cs
@@ -0,0 +1,9 @@
+﻿namespace TheRemembererDiscordBot
+{
+    public class Command
+    {
+        public virtual string? CommandName() => null;
+        public virtual string? CommandDescription() => null;
+        public virtual object? CommandAction(List<object> args) => null;
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,17 +1,22 @@
 ﻿using Discord;
+using Discord.Net;
 using Discord.WebSocket;
+using System.Text.Json;
 
-namespace RuneDungeon
+namespace TheRemembererDiscordBot
 {
     public class Program
     {
         private static Task Main() => new Program().MainAsync();
         private readonly DiscordShardedClient _client = new(DSC);
         private static readonly DiscordSocketConfig DSC = new() { LogGatewayIntentWarnings = false, MessageCacheSize = 0 };
+        public static readonly List<Command> Commands = new();
 
         private async Task MainAsync()
         {
             _client.Log += Log;
+            _client.ShardReady += ClientReady;
+            _client.SlashCommandExecuted += SlashCommandHandler;
 
             string token = File.ReadAllText("token.txt");
             await _client.LoginAsync(TokenType.Bot, token);
@@ -20,10 +25,34 @@ namespace RuneDungeon
             await Task.Delay(-1);
         }
 
-        private Task Log(LogMessage msg)
+        private static Task Log(LogMessage msg)
         {
             Console.WriteLine(msg.ToString());
             return Task.CompletedTask;
+        }
+
+        private static async Task ClientReady(DiscordSocketClient client)
+        {
+            foreach (Command command in Commands)
+            {
+                SlashCommandBuilder commandBuilder = new();
+                commandBuilder.WithName(command.CommandName());
+                commandBuilder.WithDescription(command.CommandDescription());
+                try
+                {
+                    await client.CreateGlobalApplicationCommandAsync(commandBuilder.Build());
+                }
+                catch (ApplicationCommandException exception)
+                {
+                    var json = JsonSerializer.Serialize(exception.Errors);
+                    Console.WriteLine(json);
+                }
+            }
+        }
+
+        private static async Task SlashCommandHandler(SocketSlashCommand command)
+        {
+            await command.RespondAsync($"You executed {command.Data.Name}");
         }
     }
 }
