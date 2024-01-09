ad89bfffdb28ff515a15632ecd7e7d5b99ea4df8
MarauderKnight3 <marauderknight3@gmail.com>
2024-01-08 18:02:40 -0600

    Default command behavior

--- Command.cs
+++ Command.cs
@@ -6,6 +6,15 @@ namespace TheRemembererDiscordBot
     {
         public virtual string? CommandName() => null;
         public virtual string? CommandDescription() => null;
-        public virtual async Task<object?> CommandAction(SocketMessage inputMessage, List<object> args) => null;
+        public virtual async Task<object?> CommandAction(SocketMessage inputMessage, List<object> args)
+        {
+            try
+            {
+                await inputMessage.Channel.SendMessageAsync("This is undefined command behavior.");
+            }
+            catch { }
+
+            return null;
+        }
     }
 }
