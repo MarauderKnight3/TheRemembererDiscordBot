0c37ea35521d3251cf68c53c392a1da7a84d6186
MarauderKnight3 <marauderknight3@gmail.com>
2024-01-12 19:48:23 -0600

    Update NewInventory.cs

--- Commands/NewInventory.cs
+++ Commands/NewInventory.cs
@@ -12,23 +12,36 @@ namespace TheRemembererDiscordBot.Commands
         {
             string nameOfNewInventory = args[0].ToString() ?? string.Empty;
 
-            if (string.IsNullOrWhiteSpace(nameOfNewInventory))
+            int checkStatus = CheckNewInventoryName(message, nameOfNewInventory);
+
+            if (checkStatus != 0)
             {
-                await Respond(message, "The name of the inventory cannot be whitespace or empty.");
+                await Respond(message, CheckNewInventoryNameErrorMessage(checkStatus));
                 return;
             }
 
-            SaveData thisUserData = SaveData.GetSaveData(message.Author.Id);
-
-            if (thisUserData.Inventories.ContainsKey(nameOfNewInventory))
-            {
-                await Respond(message, "You already have an inventory with that name.");
-                return;
-            }
-
-            thisUserData.Inventories.Add(nameOfNewInventory, new());
+            SaveData.GetSaveData(message.Author.Id).Inventories.Add(nameOfNewInventory, new());
 
             await Respond(message, "The inventory `" + nameOfNewInventory + "` has been created successfully.");
         }
+
+        public static int CheckNewInventoryName(SocketMessage message, string nameOfNewInventory)
+        {
+            if (string.IsNullOrWhiteSpace(nameOfNewInventory))
+                return 1;
+            if (nameOfNewInventory.Length >= 24)
+                return 2;
+            if (SaveData.GetSaveData(message.Author.Id).Inventories.ContainsKey(nameOfNewInventory))
+                return 3;
+            return 0;
+        }
+
+        public static string CheckNewInventoryNameErrorMessage(int code) => code switch
+        {
+            1 => "The name of the inventory cannot be whitespace or empty.",
+            2 => "The name of the inventory must be less than 25 characters.",
+            3 => "You already have an inventory with that name.",
+            _ => ""
+        };
     }
 }
