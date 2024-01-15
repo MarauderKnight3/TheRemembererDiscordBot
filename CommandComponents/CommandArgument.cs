d2ce50967630480e861e7aed8783782de730253c
MarauderKnight3 <marauderknight3@gmail.com>
2024-01-15 12:11:20 -0600

    fix index out of bounds

--- CommandComponents/CommandArgument.cs
+++ CommandComponents/CommandArgument.cs
@@ -46,8 +46,8 @@
                 false when ExpectedInputs.Count == 2 && ExpectedInputs[0] is int && ExpectedInputs[1] is int => ArgType.PositiveIntegerRange,
                 false when ExpectedInputs.Count > 0 && ExpectedInputs[0] is int => ArgType.PositiveIntegerRangeOrText,
                 true when ExpectedInputs.Count > 0 && ExpectedInputs[0] is int => ArgType.PositiveIntegerRangeOrTextConcat,
-                false when (ExpectedInputs.Count > 0 || forceNotCustom) && ExpectedInputs[0] is not int => ArgType.Text,
-                true when (ExpectedInputs.Count > 0 || forceNotCustom) && ExpectedInputs[0] is not int => ArgType.TextConcat,
+                false when forceNotCustom || (ExpectedInputs.Count > 0 && ExpectedInputs[0] is not int) => ArgType.Text,
+                true when forceNotCustom || (ExpectedInputs.Count > 0 && ExpectedInputs[0] is not int) => ArgType.TextConcat,
                 false when ExpectedInputs.Count == 0 => ArgType.CustomText,
                 true when ExpectedInputs.Count == 0 => ArgType.CustomTextConcat,
                 _ => ArgType.CustomText
