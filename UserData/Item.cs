d59d9863a9278b01525728e3701c02acfaabf4ac
MarauderKnight3 <marauderknight3@gmail.com>
2024-01-14 21:29:17 -0600

    fix overflow

--- UserData/Item.cs
+++ UserData/Item.cs
@@ -4,15 +4,15 @@
     {
         private readonly ItemType _Type;
         public ItemType Type { get { return _Type; } }
-        private ulong _Count;
-        public ulong Count { get { return _Count; } }
+        private long _Count;
+        public long Count { get { return _Count; } }
 
-        public Item(ItemType type, ulong count)
+        public Item(ItemType type, long count)
         {
             _Type = type;
             _Count = count;
         }
 
-        public void ModifyCount(ulong amount) => _Count = Math.Clamp(_Count + amount, 0, 1000000);
+        public void ModifyCount(long amount) => _Count = Math.Clamp(_Count + amount, 0, 1000000);
     }
 }
